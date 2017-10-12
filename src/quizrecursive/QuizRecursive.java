20a0db13e1687afd0d4d7cca6a76fca365c86e46
manok5757 <manok5757@HRH-IC0027305.wrdsb.ca>
2017-10-12 11:49:09 -0400

    Quiz complete

--- src/quizrecursive/QuizRecursive.java
+++ src/quizrecursive/QuizRecursive.java
@@ -11,41 +11,36 @@ package quizrecursive;
 public class QuizRecursive {
 
     // create a recursive method to flip a word
-    public String reverseString(String word){
+    public String reverseString(String word) {
+
+
         // create a base case if the word is 0 letters or 1 letter
-        if(word.length()==1||word.length()==0){
+        if (word.length() == 1 || word.length() == 0) {
+            // return the word
             return word;
-        }
-       
-        // the word is longer than one letter
-        else{
-            // the last letter is euqal to the last letter of the word
-            String lastLetter = word.substring(word.length()-1);
+        } // the word is longer than one letter
+        else {
+            // set the last letter is euqal to the last letter of the word
+            String lastLetter = word.substring(word.length() - 1);
             // reduce the word to make it the original word without the last letter
-            String newWord =word.substring(0,word.length()-1);
-             
-            // call in the method again with the last word as parameter
-            return reverseString(lastLetter)+newWord;
-             
-            
-            
-            
+            String newWord = word.substring(0, word.length() - 1);
+
+            // call in the method again with the new short word as parameter and the last letter
+            return lastLetter + reverseString(newWord);
+
+
+
+
         }
     }
-    
-    
-    
-    
-    
-    
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // test the method
         QuizRecursive test = new QuizRecursive();
-         
-        String recursive=test.reverseString("book");
-        System.out.println(recursive);
+        // output the method
+        System.out.println(test.reverseString("basses"));
     }
 }
